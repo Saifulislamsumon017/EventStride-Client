b21459288b02bfe6d3709cb6208a88de5b4924c0
Saiful Islam <sumon.sis169@gmail.com>
2025-10-25 22:59:46 +0600

    dashboard overview done

--- src/components/AllComponents/DashboardComponets/ChartCard.jsx
+++ src/components/AllComponents/DashboardComponets/ChartCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ChartCard = ({ title, children }) => {
   return (
-    <div className="bg-white rounded-2xl shadow p-5 hover:shadow-lg transition-all duration-300">
-      <h2 className="text-lg font-semibold mb-4 text-gray-800">{title}</h2>
+    <div className=" rounded-2xl shadow border p-5 hover:shadow-lg transition-all duration-300">
+      <h2 className="text-lg font-semibold mb-4 ">{title}</h2>
       {children}
     </div>
   );
